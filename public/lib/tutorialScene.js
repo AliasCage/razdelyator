ab7a48f5ad1dada84e0bc7d3d166ea0d4f4e6eee
aliasCage <g.triquivel@gmail.com>
2020-05-25 00:11:59 +0300

    Fix load video

--- public/lib/tutorialScene.js
+++ public/lib/tutorialScene.js
@@ -30,6 +30,8 @@ var Tutorial = new Phaser.Class({
         });
 
 
+        this.load.setBaseURL('img');
+
         this.load.video('tut_video_2', 'tutorial/tut_2.mp4', 'loadeddata', false, true);
         this.load.video('tut_video_3', 'tutorial/tut_3.mp4', 'loadeddata', false, true);
         this.load.video('tut_video_4', 'tutorial/tut_4.mp4', 'loadeddata', false, true);
